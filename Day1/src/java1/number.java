85182d635f8cec1d586375d42c07c91e3322c095
NavinKumar77 <89631106+NavinKumar77@users.noreply.github.com>
2023-02-07 09:51:28 +0530

    Add files via upload

---
+++ Day1/src/java1/number.java
@@ -0,0 +1,17 @@
+package java1;
+
+public class number {
+	public static void main(String[]args) {
+		int n = 1;
+		if(n>0) {
+			System.out.println("positive");
+		}
+		else if(n<0) {
+			System.out.println("negative");
+		}
+		else {
+			System.out.println("zero");
+		}
+	}
+
+}
